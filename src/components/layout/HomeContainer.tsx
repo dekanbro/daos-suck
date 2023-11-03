5bc1f649c3c5475c6d632f70a9eb1e64fad751a7
dekanbro <dekanbrown@odyssy.io>
2023-11-03 12:04:10 -0600

    go back to yarn in action

--- src/components/layout/HomeContainer.tsx
+++ src/components/layout/HomeContainer.tsx
@@ -3,13 +3,18 @@ import { Footer, H4 } from "@daohaus/ui";
 import { Outlet, useLocation } from "react-router-dom";
 import { TARGET_DAO } from "../../targetDao";
 import { TXBuilder } from "@daohaus/tx-builder";
+import { styled } from "styled-components";
 
 export const HomeContainer = () => {
   const location = useLocation();
   const { publicClient, address } = useDHConnect();
 
+  const DHLayoutWrapper = styled(DHLayout)`
+margin-top: -200px;
+    `
+
   return (
-    <DHLayout navLinks={[]} pathname={location.pathname}>
+    <DHLayoutWrapper navLinks={[]} pathname={location.pathname}>
       <TXBuilder
         publicClient={publicClient}
         chainId={TARGET_DAO.CHAIN_ID}
@@ -17,6 +22,6 @@ export const HomeContainer = () => {
       >
         <Outlet />
       </TXBuilder>
-    </DHLayout>
+    </DHLayoutWrapper>
   );
 };
